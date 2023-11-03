a46b473cb9a0b776e1d72748115a9929ae062d5d
Gonzalo115 <3513074510101@ingenieria.usac.edu.gt>
2023-11-02 23:31:51 -0600

    11. Ultimo

---
+++ Backend/Fecha.py
@@ -0,0 +1,9 @@
+class Fecha():
+    def __init__(self, fecha, mesajesPositivos, mesajesNegativos, mesajesNeutros) -> None:
+        self.fecha = fecha
+        self.mesajesPositivos = mesajesPositivos
+        self.mesajesNegativos = mesajesNegativos
+        self.mesajesNeutros = mesajesNeutros
+        self.hashtags = []
+        self.mensiones = []
+
